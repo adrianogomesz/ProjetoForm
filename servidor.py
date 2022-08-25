e4abd40c3107a12e57bb5d737e10a572ccff7433
Adriano Gomes <adrianodede76@gmail.com>
2022-08-25 15:34:22 -0300

    firstcommit

---
+++ servidor.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+
+HOST = 'localhost'
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+    print("Servidor iniciado em: ", PORT)
+    httpd.serve_forever()
